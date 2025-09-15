665a9b8d52c08dab7890d4fbb84f17090065db86
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-09-15 16:58:10 +0700

    feat: add resource with filament

---
+++ app/Filament/Resources/Foods/Schemas/FoodsForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Foods\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class FoodsForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('description')
+                    ->required(),
+                FileUpload::make('image')
+                    ->image()
+                    ->required(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                TextInput::make('price_afterdiscount')
+                    ->default(null),
+                TextInput::make('percent')
+                    ->default(null),
+                TextInput::make('is_promo')
+                    ->default(null),
+                TextInput::make('categories_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Foods/Tables/FoodsTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\Foods\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class FoodsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->searchable(),
+                ImageColumn::make('image'),
+                TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                TextColumn::make('price_afterdiscount')
+                    ->searchable(),
+                TextColumn::make('percent')
+                    ->searchable(),
+                TextColumn::make('is_promo')
+                    ->searchable(),
+                TextColumn::make('categories_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
